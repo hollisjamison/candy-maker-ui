036c494966512ade9396e10a65310d93b949f59b
Hollis <hollisjamison@gmail.com>
2021-09-09 23:12:11 -0400

    Answer to part two

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -1,58 +1,7 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+import './search.css'
 
-const Search = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [manufacturers, setManufacturers] = useState([])
-
-  useEffect(() => {
-    fetchManufacturers()
-  }, []) 
-
-  const fetchManufacturers = async () => {
-    let {data} = await axios.get('http://localhost:1337/api/manufacturers')
-    console.log(data)
-    setManufacturers(data)
-  }
-
-  return (
-    <div className="content">
-      <div className="title">Candy Makers</div>
-      <input
-        className="searchBox"
-        type="text"
-        name="search"
-        value={searchTerm}
-        onChange={(event) => setSearchTerm(event.target.value)}
-      />
-      {
-        manufacturers.filter(manufacturer => manufacturer.name.toLowerCase().includes(searchTerm.toLowerCase())).map(manufacturer => {
-          return (
-            <div>{manufacturer.name} ({manufacturer.country})</div>
-          )
-        })
-      }
-
-    </div>
-  );
-};
-
-export default Search
-=======
-import React, { useState } from 'react'
-
-const Search = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  return (
-    <div className="content">
-    <div className="title">Candy Makers</div>
-    <input className="searchBox" type="text" name="search" value={searchTerm} onChange={event => setSearchTerm(event.target.value)} />
-    <div className="searchTerm">{searchTerm}</div>
-    </div>
-  )
-  }
-
-export default Search
->>>>>>> Solution for part one
+export default ({ term, setter }) => (
+  <input type="text" name="search" value={term} onChange={event => setter(event.target.value)} />
+)
